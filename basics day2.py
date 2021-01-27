4736dac744210165c1b4e55430c0b661d11d8f2a
Pawan kumar <60340211+gitcoder-pawan@users.noreply.github.com>
2021-01-27 16:00:49 +0530

    pycharm

---
+++ basics day2.py
@@ -0,0 +1,41 @@
+"""
+a='pawan '
+b=' kumar'
+c=a+b
+print(c)
+print(c+" 3")
+print(a*3)
+print(str(2/2) + "  float division")
+
+# name = input("enter your name : ")
+# print(name)
+# num_1= str(3)
+# num_2=float(num_1)
+# num_3=int(num_1)
+# # print(num_2+num_3)
+# # print(int(num_2))
+# name, age = "pawan" , 20
+# print(name+ " age is "+ str(age))
+# name , age = input("enter your name and age :   ").split("$")
+# # print("hello {} your age is {}".format(name,age))
+# print(f"hello {name} your age is {age}")
+# num_1, num_2, num_3= input ("enter three no :").split()
+# avg= (int(num_1)+int(num_2)+int(num_3))/3
+# print (f"average of {num_1} {num_2} {num_3} is {avg}  ")
+# string slicing
+# lang= "python"
+# print(lang[5] + lang[-1])
+# print(lang[-6:])
+# print(lang[0:6])
+# print(lang[1:6:2])
+# print
+# print(lang[-2::-2])
+# user_name=input("enter user name :  ")
+# print(user_name[::-1])
+# name =" pAWan kumar"
+# print(len(name))
+# print(name.lower())
+# print(name.upper())
+# print(name.title())
+# print(name.count("a"))
+print(name.count("A"))"""

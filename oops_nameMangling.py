fcf1fdb668be5f5b5493dbb3a5ca85b509f27c8b
Pawan kumar <60340211+gitcoder-pawan@users.noreply.github.com>
2021-03-16 21:22:56 +0530

    Add files via upload

--- oops_nameMangling.py
+++ oops_nameMangling.py
@@ -1,21 +1,28 @@
 class User:
     def __init__(self, name, mob_no):
-        self._name=name
-        self._mob_no=mob_no
-    def show_details(self):
-        self._name=10
-        self._mob_no="x"
-        return  f"{self._name *2} {self._mob_no}"
+        self._name = name
+        self._mob_no = mob_no
 
-class Employee(User):
+    def show_details(self):
+        self._name = 10
+        self._mob_no = "x"
+        return f"{self._name *2} {self._mob_no}"
+
+
+class Employee (User):
     def __init__(self, name, mob_no, eid):
         super().__init__(name, mob_no)
-        self.eid=eid
+        self.eid = eid
+
     def show_details(self):
         return f"{self.eid} : {self._name} - {self._mob_no}"
 
-user1=User('pawan kumar ', 6205244673)
-emp1=Employee('pawan kumar', 6205244673,1)
+
+# noinspection SpellCheckingInspection
+user1 = User('pawan kumar ', 6205244673)
+# noinspection SpellCheckingInspection
+emp1 = Employee('pawan kumar ', 6205244673, 1)
+
 print(dir(user1))
 print(user1.show_details())
 print(emp1.show_details())
